eb114261239091e67c07c8368e6710693569129f
ticklydo <flajsovan@email.com>
2026-05-24 18:47:41 +0200

    odstranenie redirect loop

--- next.config.ts
+++ next.config.ts
@@ -1,16 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  async redirects() {
-    return [
-      {
-        source: "/(.*)",
-        has: [{ type: "host", value: "www.ticklydo.com" }],
-        destination: "https://ticklydo.com/:path*",
-        permanent: true,
-      },
-    ];
-  },
   async headers() {
     return [
       {
