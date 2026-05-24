8df83d5d69c77d35eff1120d5500a2b0c1a14714
ticklydo <flajsovan@email.com>
2026-05-24 18:30:15 +0200

    www redirect fix

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: "/(.*)",
+        has: [{ type: "host", value: "www.ticklydo.com" }],
+        destination: "https://ticklydo.com/:path*",
+        permanent: true,
+      },
+    ];
+  },
   async headers() {
     return [
       {
